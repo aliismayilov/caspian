523dafa5d39b0b08ac17a4251abfd7cb10123d4c
Ali Ismayilov <ali@ismailov.info>
2013-04-05 22:17:54 +0500

    add django filebrowser

--- caspian/settings.py
+++ caspian/settings.py
@@ -119,6 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'grappelli',
+    'filebrowser',
     'django.contrib.admin',
 )
 
--- caspian/urls.py
+++ caspian/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from filebrowser.sites import site
 
 from django.contrib import admin
 admin.autodiscover()
@@ -6,4 +7,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     (r'^grappelli/', include('grappelli.urls')),
+    url(r'^admin/filebrowser/', include(site.urls)),
 )
